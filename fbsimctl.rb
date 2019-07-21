43fd40caa05bc8382b579dadd68d1359a80c6899
Vyacheslav Frolov <vyacheslav.frolov@corp.badoo.com>
2019-07-21 02:29:04 +0100

    Bottle for forked repo of fbsimctl HEAD-08451d7 branch 'badoo'

---
+++ fbsimctl.rb
@@ -0,0 +1,30 @@
+class Fbsimctl < Formula
+  desc "A Powerful Command Line tool for automating iOS Simulators"
+  homepage "https://github.com/badoo/FBSimulatorControl"
+  url "https://github.com/badoo/FBSimulatorControl/archive/08451d7.tar.gz"
+  sha256 "2c650fd17664fb576c450c7b55c49832b19c9faf689d175aa524142b2d0a8b2c"
+  head "https://github.com/badoo/FBSimulatorControl.git"
+  version "HEAD-08451d7"
+  revision 0
+
+  bottle do
+    root_url "https://github.com/badoo/homebrew-qa-bottles/releases/download/v1.0"
+    cellar :any
+    sha256 "0666d83e496616d01ebf56720fb31d6a35e2dd11dc1eb9da13b6e2e1f646f75b" => :mojave
+  end
+
+  depends_on "carthage"
+  depends_on :xcode => ["10.0", :build]
+
+  def install
+    system "./build.sh", "fbsimctl", "build", prefix
+  end
+
+  pour_bottle? do
+    true
+  end
+
+  test do
+    system "fbsimctl", "list"
+  end
+end
