56fc86bcc3849206e48a0b4d417c7dc1f405724f
Vyacheslav Frolov <frolov78@gmail.com>
2019-09-25 13:29:08 +0100

    Update fbsimctl iOS 13.1

--- fbsimctl.rb
+++ fbsimctl.rb
@@ -1,17 +1,17 @@
 class Fbsimctl < Formula
   desc "A Powerful Command Line tool for automating iOS Simulators"
   homepage "https://github.com/badoo/FBSimulatorControl"
-  url "https://github.com/badoo/FBSimulatorControl/archive/e5fbeb7.tar.gz"
-  sha256 "3b0bc9cffe6e31d6e44b9b47133bb9a5e61a8bbf86f7275be2a3b7418c61685f"
+  url "https://github.com/badoo/FBSimulatorControl/archive/d6edcb1.tar.gz"
+  sha256 "486bdc39bdc4addb87b4e2f40c00ff865ea3b81a8d067f1ac60d838a1f2c90ac"
   head "https://github.com/badoo/FBSimulatorControl.git", branch: "badoo"
-  version "HEAD-e5fbeb7"
+  version "HEAD-d6edcb1"
 
   bottle do
     root_url "https://github.com/badoo/homebrew-qa-bottles/releases/download/v1.0"
     cellar :any_skip_relocation
-    sha256 "eab2a8ca01c5f68ef5daf6b37a9cad97d9a64b252dafaadb9c5c253e07823b97" => :catalina
-    sha256 "eab2a8ca01c5f68ef5daf6b37a9cad97d9a64b252dafaadb9c5c253e07823b97" => :mojave
-    sha256 "eab2a8ca01c5f68ef5daf6b37a9cad97d9a64b252dafaadb9c5c253e07823b97" => :high_sierra
+    sha256 "0a753968dabc5881afdef24159dcd8d338285bc124cb699c686a60ad1efb7a47" => :catalina
+    sha256 "0a753968dabc5881afdef24159dcd8d338285bc124cb699c686a60ad1efb7a47" => :mojave
+    sha256 "0a753968dabc5881afdef24159dcd8d338285bc124cb699c686a60ad1efb7a47" => :high_sierra
   end
 
   depends_on "carthage"
