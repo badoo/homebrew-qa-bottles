b9907e1ec162e27a7bce70d16dff2f8f476ea339
Vyacheslav Frolov <frolov78@gmail.com>
2021-02-02 22:26:14 +0000

    Add iOS 14.4 support

--- fbsimctl.rb
+++ fbsimctl.rb
@@ -1,17 +1,17 @@
 class Fbsimctl < Formula
   desc "A Powerful Command Line tool for automating iOS Simulators"
   homepage "https://github.com/badoo/FBSimulatorControl"
-  url "https://github.com/badoo/FBSimulatorControl/archive/d318e0e.tar.gz"
-  sha256 "4c4c26d65db4d8fb2b0b79eacb3daee718216f1f3e350fcf591b82247ed9cfc6"
+  url "https://github.com/badoo/FBSimulatorControl/archive/d6d99e5.tar.gz"
+  sha256 "50af6f1bb1e830a24d79dbf27484433e873b9ee24b4a02c2500335c3879a3ddf"
   head "https://github.com/badoo/FBSimulatorControl.git", branch: "badoo"
-  version "HEAD-d318e0e"
+  version "HEAD-d6d99e5"
 
   bottle do
     root_url "https://github.com/badoo/homebrew-qa-bottles/releases/download/v1.0"
     cellar :any_skip_relocation
-    sha256 "10613870e4a51dddccec5e5ae600a2b166524a8f424bffc960ae23c69cfbd99a" => :catalina
-    sha256 "10613870e4a51dddccec5e5ae600a2b166524a8f424bffc960ae23c69cfbd99a" => :big_sur
-    sha256 "10613870e4a51dddccec5e5ae600a2b166524a8f424bffc960ae23c69cfbd99a" => :mojave
+    sha256 "b4363a67733461c96817878b3a9b3521e7f5afecc1d930f2ce1c9765dfedd5c7" => :catalina
+    sha256 "b4363a67733461c96817878b3a9b3521e7f5afecc1d930f2ce1c9765dfedd5c7" => :big_sur
+    sha256 "b4363a67733461c96817878b3a9b3521e7f5afecc1d930f2ce1c9765dfedd5c7" => :mojave
   end
 
   def install
