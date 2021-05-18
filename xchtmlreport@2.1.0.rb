8ffc2e37af5ef54a819837b7b80a69a6145c7c37
Vyacheslav Frolov <vyacheslav.frolov@corp.badoo.com>
2021-05-18 23:24:08 +0100

    Add bottles for XCTestHTMLReport 2.1.0

--- xchtmlreport@2.1.0.rb
+++ xchtmlreport@2.1.0.rb
@@ -8,6 +8,18 @@ class XchtmlreportAT210 < Formula
   sha256 "98bc4d97c18b7bfa46ca9fbe72df2351fa96acabe70f14ec5b4f9aeca7bc5b57"
   head "https://github.com/TitouanVanBelle/XCTestHTMLReport.git", branch: "develop"
 
+  bottle do
+    root_url "https://github.com/badoo/homebrew-qa-bottles/releases/download/v1.0"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, catalina: "0f5e96311a7aea3800c5c4424412b9c6df87a8701f569e63a5ce09197041623a"
+    sha256 cellar: :any_skip_relocation, big_sur: "0f5e96311a7aea3800c5c4424412b9c6df87a8701f569e63a5ce09197041623a"
+    sha256 cellar: :any_skip_relocation, mojave: "0f5e96311a7aea3800c5c4424412b9c6df87a8701f569e63a5ce09197041623a"
+  end
+
+  pour_bottle? do
+    true
+  end
+
   def install
     system "swift", "build", "--disable-sandbox", "-c", "release"
     bin.install ".build/release/xchtmlreport"
