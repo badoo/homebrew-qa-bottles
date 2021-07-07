c10ce715e6225f34fe0c238f2566232a7676f825
Vyacheslav Frolov <frolov78@gmail.com>
2021-07-07 10:13:29 +0100

    Update fbsimctl.rb

--- fbsimctl.rb
+++ fbsimctl.rb
@@ -4,16 +4,19 @@
 class Fbsimctl < Formula
   desc "Powerful Command-line tool for automating iOS Simulators"
   homepage "https://github.com/badoo/FBSimulatorControl"
-  url "https://github.com/badoo/FBSimulatorControl/archive/d6d99e5.tar.gz"
-  version "HEAD-d6d99e5"
-  sha256 "50af6f1bb1e830a24d79dbf27484433e873b9ee24b4a02c2500335c3879a3ddf"
+  url "https://github.com/badoo/FBSimulatorControl/archive/b6c1216.tar.gz"
+  version "HEAD-b6c1216"
+  sha256 "1135b44294dcc976419a92994b6683e783d192f47ac0964a7dd8631d38274432"
   head "https://github.com/badoo/FBSimulatorControl.git", branch: "badoo"
 
   bottle do
     root_url "https://github.com/badoo/homebrew-qa-bottles/releases/download/v1.0"
-    sha256 cellar: :any_skip_relocation, catalina: "b4363a67733461c96817878b3a9b3521e7f5afecc1d930f2ce1c9765dfedd5c7"
-    sha256 cellar: :any_skip_relocation, big_sur:  "b4363a67733461c96817878b3a9b3521e7f5afecc1d930f2ce1c9765dfedd5c7"
-    sha256 cellar: :any_skip_relocation, mojave:   "b4363a67733461c96817878b3a9b3521e7f5afecc1d930f2ce1c9765dfedd5c7"
+    sha256 cellar: :any_skip_relocation, big_sur:  "fb92723d0a1de4fbe5daafa6d7a1ddb1dfa65dab94562015c312b5f9a89ec2f8"
+    sha256 cellar: :any_skip_relocation, catalina: "fb92723d0a1de4fbe5daafa6d7a1ddb1dfa65dab94562015c312b5f9a89ec2f8"
+    sha256 cellar: :any_skip_relocation, monterey: "fb92723d0a1de4fbe5daafa6d7a1ddb1dfa65dab94562015c312b5f9a89ec2f8"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "143819ea2fc90ba390069b85a37aa5181822d3a4acda27b800ba4e45e5e1d8e3"
+    sha256 cellar: :any_skip_relocation, arm64_catalina: "143819ea2fc90ba390069b85a37aa5181822d3a4acda27b800ba4e45e5e1d8e3"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "143819ea2fc90ba390069b85a37aa5181822d3a4acda27b800ba4e45e5e1d8e3"
   end
 
   pour_bottle? do
