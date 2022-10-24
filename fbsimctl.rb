426f42e4bf113d46bc041045986ff0c109d3d14d
Vyacheslav Frolov <frolov78@gmail.com>
2022-10-24 16:11:30 +0100

    Update fbsimctl to 0026866 with iPhone 13x and 14x

--- fbsimctl.rb
+++ fbsimctl.rb
@@ -4,19 +4,19 @@
 class Fbsimctl < Formula
   desc "Powerful Command-line tool for automating iOS Simulators"
   homepage "https://github.com/badoo/FBSimulatorControl"
-  url "https://github.com/badoo/FBSimulatorControl/archive/b6c1216.tar.gz"
-  version "HEAD-b6c1216"
-  sha256 "1135b44294dcc976419a92994b6683e783d192f47ac0964a7dd8631d38274432"
+  url "https://github.com/badoo/FBSimulatorControl/archive/refs/tags/0026866.tar.gz"
+  version "HEAD-0026866"
+  sha256 "ee17df66a8f89717e621efdce167367679439505564e89838f54f2c00a721bce"
   head "https://github.com/badoo/FBSimulatorControl.git", branch: "badoo"
 
   bottle do
     root_url "https://github.com/badoo/homebrew-qa-bottles/releases/download/v1.0"
-    sha256 cellar: :any_skip_relocation, big_sur:  "fb92723d0a1de4fbe5daafa6d7a1ddb1dfa65dab94562015c312b5f9a89ec2f8"
-    sha256 cellar: :any_skip_relocation, catalina: "fb92723d0a1de4fbe5daafa6d7a1ddb1dfa65dab94562015c312b5f9a89ec2f8"
-    sha256 cellar: :any_skip_relocation, monterey: "fb92723d0a1de4fbe5daafa6d7a1ddb1dfa65dab94562015c312b5f9a89ec2f8"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "143819ea2fc90ba390069b85a37aa5181822d3a4acda27b800ba4e45e5e1d8e3"
-    sha256 cellar: :any_skip_relocation, arm64_catalina: "143819ea2fc90ba390069b85a37aa5181822d3a4acda27b800ba4e45e5e1d8e3"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "143819ea2fc90ba390069b85a37aa5181822d3a4acda27b800ba4e45e5e1d8e3"
+    sha256 cellar: :any_skip_relocation, big_sur:  "539e8ee9fa074979541773070c6ffd01966afeaa78cec01f7509d315a7d58d3f"
+    sha256 cellar: :any_skip_relocation, monterey: "539e8ee9fa074979541773070c6ffd01966afeaa78cec01f7509d315a7d58d3f"
+    sha256 cellar: :any_skip_relocation, ventura: "539e8ee9fa074979541773070c6ffd01966afeaa78cec01f7509d315a7d58d3f"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "539e8ee9fa074979541773070c6ffd01966afeaa78cec01f7509d315a7d58d3f"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "539e8ee9fa074979541773070c6ffd01966afeaa78cec01f7509d315a7d58d3f"
+    sha256 cellar: :any_skip_relocation, arm64_ventura: "539e8ee9fa074979541773070c6ffd01966afeaa78cec01f7509d315a7d58d3f"
   end
 
   pour_bottle? do
